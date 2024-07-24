7514ff597cc84dc877a9ead6c861302fd717e725
JangJaehyeonn <wogys1520@gmail.com>
2024-07-24 10:00:23 +0900

    ♻️refactor - CU-38 게시물 CRUD 수정

--- src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepository.java
+++ src/main/java/com/team9oogling/codyus/domain/post/repository/PostRepository.java
@@ -1,10 +1,12 @@
 package com.team9oogling.codyus.domain.post.repository;
 
 import com.team9oogling.codyus.domain.post.entity.Post;
+import com.team9oogling.codyus.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+    List<Post> findByUser(User user);
 }
