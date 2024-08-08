d78464db89a2b05bcbdeee9b94483e37ea228f5a
nenney <a01056499749@gmail.com>
2024-08-08 18:20:43 +0900

    ✨ update - CU-70 푸터 적용

--- src/main/java/com/team9oogling/codyus/global/config/SecurityConfig.java
+++ src/main/java/com/team9oogling/codyus/global/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.team9oogling.codyus.global.config;
 
 import com.team9oogling.codyus.domain.user.repository.UserRepository;
-import com.team9oogling.codyus.global.security.UserDetailsServiceImpl;
 import com.team9oogling.codyus.domain.user.service.UserService;
 import com.team9oogling.codyus.global.dto.SecurityResponse;
 import com.team9oogling.codyus.global.jwt.JwtProvider;
 import com.team9oogling.codyus.global.security.CustomAuthenticationEntryPoint;
 import com.team9oogling.codyus.global.security.JwtAuthenticationFilter;
 import com.team9oogling.codyus.global.security.JwtAuthorizationFilter;
+import com.team9oogling.codyus.global.security.UserDetailsServiceImpl;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,9 +33,9 @@ public class SecurityConfig {
   private final UserService userService;
 
   public SecurityConfig(JwtProvider jwtProvider, UserDetailsServiceImpl userDetailsService,
-                        AuthenticationConfiguration authenticationConfiguration, UserRepository userRepository,
-                        CustomAuthenticationEntryPoint customAuthenticationEntryPoint,
-                        SecurityResponse securityResponse, UserService userService) {
+      AuthenticationConfiguration authenticationConfiguration, UserRepository userRepository,
+      CustomAuthenticationEntryPoint customAuthenticationEntryPoint,
+      SecurityResponse securityResponse, UserService userService) {
     this.jwtProvider = jwtProvider;
     this.userDetailsService = userDetailsService;
     this.authenticationConfiguration = authenticationConfiguration;
@@ -47,7 +47,7 @@ public class SecurityConfig {
 
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-          throws Exception {
+      throws Exception {
     return configuration.getAuthenticationManager();
   }
 
@@ -55,7 +55,7 @@ public class SecurityConfig {
   public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
 
     JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtProvider, userRepository,
-            securityResponse);
+        securityResponse);
     filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
 
     return filter;
@@ -63,7 +63,8 @@ public class SecurityConfig {
 
   @Bean
   public JwtAuthorizationFilter jwtAuthorizationFilter() {
-    return new JwtAuthorizationFilter(jwtProvider, userDetailsService, securityResponse, userService);
+    return new JwtAuthorizationFilter(jwtProvider, userDetailsService, securityResponse,
+        userService);
   }
 
   @Bean
@@ -72,23 +73,27 @@ public class SecurityConfig {
     http.csrf((csrf) -> csrf.disable());
 
     http.sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                    // 1. 중복 url 삭제 2. 파일명 -> API 로 변경
-                    .requestMatchers("/api/users/signup", "/main.html","login.html"
-                            ,"/posts","/api/posts","posts/postDetail/", "/chat", "/chatting/**","/posts/**", "/posts/postCreate",
-                            "/api/users/token/refresh", "/api/users/login", "/login-page", "/home",
-                            "/api/user-info", "/signup-page", "/shop-page","/searchResult.html","api/posts/search").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/user/kakao/callback", "/api/posts", "/api/users/email",
-                            "/api/user-info","/login-page","/api/posts/{postId}","/posts/search","/posts/postCreate","api/posts/search/**").permitAll()
-                    .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
-                    .anyRequest().authenticated())
-            .exceptionHandling((exceptionHandling) -> {
-              exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint);
-            })
-            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+            SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            // 1. 중복 url 삭제 2. 파일명 -> API 로 변경
+            .requestMatchers("/api/users/signup", "/main.html"
+                , "/posts", "/api/posts", "posts/postDetail/", "/chatting/**", "/posts/**",
+                "/posts/postCreate",
+                "/api/users/token/refresh", "/api/users/login", "/login", "/home",
+                "/api/user-info", "/signup", "/shop", "/searchResult.html", "api/posts/search")
+            .permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/user/kakao/callback", "/api/posts",
+                "/api/users/email",
+                "/api/user-info", "/login", "/api/posts/{postId}", "/posts/search",
+                "/posts/postCreate", "api/posts/search/**").permitAll()
+            .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
+            .anyRequest().authenticated())
+        .exceptionHandling((exceptionHandling) -> {
+          exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint);
+        })
+        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
 
     return http.build();
   }
