5a940d24c25cb99e69dc75a7400f82eeb56d2c12
JangJaehyeonn <wogys1520@gmail.com>
2024-08-09 21:55:53 +0900

    🎉update - CU-75 게시물 좋아요수 결과 조회

---
+++ src/main/java/com/team9oogling/codyus/domain/like/dto/LikeCountResponseDto.java
@@ -0,0 +1,10 @@
+package com.team9oogling.codyus.domain.like.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LikeCountResponseDto {
+    private int likeCount;
+}
